b17b29d93d6f772d40721d517592127142d8bd40
Ramon Moraes - Dart <ramon.moraes94@gmail.com>
2020-07-10 20:19:03 -0300

    remove some overkill log

--- throttler/rate.go
+++ throttler/rate.go
@@ -10,26 +10,22 @@ type RateThrottler struct {
 	channel chan *http.Request
 }
 
-func (rt RateThrottler) Setup() {
-	fmt.Printf("%p\n", &rt)
+func (rt *RateThrottler) Setup() {
 	reqChannel := make(chan *http.Request)
 	rt.channel = reqChannel
 	go rt.dispatchRequests(reqChannel)
 }
 
-func (rt RateThrottler) Throttle(req *http.Request) {
-	fmt.Printf("*%p\n", &rt)
+func (rt *RateThrottler) Throttle(req *http.Request) {
 	rt.channel <- req
 }
 
-func (rt RateThrottler) dispatchRequests(c chan *http.Request) {
+func (rt *RateThrottler) dispatchRequests(c chan *http.Request) {
 	for {
 		req, ok := <-c
 		if !ok {
 			fmt.Println("NOT OKAY MAN, NOT OKAY")
 		}
-		fmt.Println("Received")
-
 		err := forwardRequest(req, rt.Address)
 		if err != nil {
 			fmt.Println("[ERROR]\n", err)
